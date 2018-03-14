896bbf97eea4b6b533503cd903894d01f18a439f
Your Name <you@example.com>
2018-03-14 23:15:12 +0400

    New Files Were Added

---
+++ public/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BraveDave</title>
+
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Cuprum|Raleway" rel="stylesheet">
+</head>
+<body>
+
+    <div class="center-text">
+        <div class="date-wrapper">
+            <div id="timer"></div>
+            <div id="date"></div>
+            <div id="month-day"></div>
+        </div>
+        <div class="nick">_BraveDave_</div>
+        <div class="p-l">HTML | CSS | JS </div>
+        <div class="social">
+            <p><a href="http://vk.com/davo8448" target="_blank">VK</a> | <a href="https://www.instagram.com/_davo8448_/" target="_blank"> Instagram</a> | <a href="https://mail.google.com/mail/u/0/#inbox?compose=16225e5630385857" target="_blank">Email</a>
+        </div>
+    </div>
+
+    <p class="song-name">Song : Bishop Briggs – River</p>
+
+    <audio loop autoplay src="audio.mp3" type="audio/mpeg">
+    <script src="script.js"></script>
+</body>
+</html>
