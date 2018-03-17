b4574176be9af2cfcc809385c91488f8d46407d6
Your Name <you@example.com>
2018-03-17 19:44:43 +0400

    New Version Of The Site Was Added.

--- public/index.php
+++ public/index.php
@@ -4,29 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>BraveDave</title>
-
-    <link rel="stylesheet" href="style.css">
+    <title>> BraveDave <</title>
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="styles/style.css">
     <link href="https://fonts.googleapis.com/css?family=Cuprum|Raleway" rel="stylesheet">
 </head>
 <body>
-
+<div class="main_wrapper">
     <div class="center-text">
         <div class="date-wrapper">
             <div id="timer"></div>
             <div id="date"></div>
             <div id="month-day"></div>
         </div>
-        <div class="nick">_BraveDave_</div>
+        <div class="nick">BraveDave</div>
         <div class="p-l">HTML | CSS | JS </div>
         <div class="social">
-            <p><a href="http://vk.com/davo8448" target="_blank">VK</a> | <a href="https://www.instagram.com/_davo8448_/" target="_blank"> Instagram</a> | <a href="https://mail.google.com/mail/u/0/#inbox?compose=16225e5630385857" target="_blank">Email</a>
+            <p>|<a href="http://vk.com/davo8448" target="_blank">VK</a> | <a href="https://www.instagram.com/_davo8448_/" target="_blank"> Instagram</a> | <a href="https://mail.google.com/mail/u/0/#inbox?compose=16225e5630385857" target="_blank">Email</a>|</p>
         </div>
     </div>
-
+</div>
     <p class="song-name">Song : Bishop Briggs – River</p>
+    <p class="copy-text">Site Idea : <a href="https://butowka.ru/" target="_blank">Butowka</a></p>
 
-    <audio loop autoplay src="audio.mp3" type="audio/mpeg">
-    <script src="script.js"></script>
+    <audio loop autoplay src="media/audio.mp3" type="audio/mpeg">
+    <script src="scripts/script.js"></script>
 </body>
 </html>
